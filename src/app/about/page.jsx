658edb4bc5fc01a41fb2e569cb2237697d96c2f6
chiedozieu <chiedozieugwor@gmail.com>
2024-05-17 14:19:09 +0100

    about page created

---
+++ src/app/about/page.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='max-w-6xl mx-auto space-y-4'>
+        <h1 className='text-2xl text-amber-600 font-medium'>About</h1>
+
+        <p>
+            At IMDb Clone, we're passionate about bringing the magic of cinema to life. Our story began with a group of film enthusiasts who wanted to create a platform where movie lovers could come together to discover, discuss, and celebrate their favorite films. Today, we're proud to be a leading online destination for movie news, reviews, and recommendations, serving a community of millions of users worldwide.
+        </p>
+
+        <p>
+            Our mission is to inspire and empower our users to explore the world of cinema, from blockbuster hits to indie gems. We believe that movies have the power to transport us, to make us laugh and cry, and to bring us closer together. That's why we're dedicated to providing the most comprehensive and engaging movie content, including in-depth reviews, exclusive interviews, and behind-the-scenes features. Whether you're a casual moviegoer or a die-hard cinephile, we're committed to helping you find your next great cinematic experience.
+        </p>
+
+        <p>
+            As we continue to grow and evolve, we remain true to our core values of passion, innovation, and community. We're committed to fostering a welcoming and inclusive environment where movie fans can share their thoughts, opinions, and experiences. We're also dedicated to supporting the film industry and promoting diversity, equity, and inclusion in all aspects of our business. Thank you for joining us on this cinematic journey – we can't wait to see what's next!
+        </p>
+    </div>
+  )
+}
